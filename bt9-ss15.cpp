29a8f50e39ec623b21337a8e7526d8fed210201c
nguyen0605 <laotuyentvm@gmail.com>
2025-01-02 15:13:21 +0700

    Add files via upload

--- bt9-ss15.cpp
+++ bt9-ss15.cpp
@@ -5,15 +5,23 @@ int main(){
 	char result[50];
 	char ch;
 	int j=0;
+	int found=0;
+	printf("Chuoi ban dau: %s\n",str);
 	printf("Nhap vao ki tu muon xoa: ");
-	scanf("%c",&ch);
+	scanf(" %c",&ch);
 	for(int i=0;i<strlen(str);i++){
 		if(str[i]!=ch){
 			result[j]=str[i];
 			j++;
+		}else{
+			found=1;
 		}
 	}
 	result[j]='\0';
-	printf("Chuoi sau khi xoa ki tu %c: %s",ch,result);
+	if(found){
+		printf("Chuoi sau khi xoa ki tu %c la %s",ch,result);
+	}else{
+		printf("Ki tu %c khong ton tai trong chuoi!",ch);
+	}
 	return 0;
 }
